98f6cf3bb50f38a792a6fe6f96d2912a77851d21
roeishou <roeis@gini-apps.com>
2023-01-05 13:03:35 +0200

    wip

--- src/app/_shared/modules/auth/utilites/is-jwt-expired.ts
+++ src/app/_shared/modules/auth/utilites/is-jwt-expired.ts
@@ -1,33 +1,47 @@
+import { useUserData } from './../../../../../store/useUserData';
 import { useSystemData } from 'store/useSystemData';
 import {getJwtExpirationDate} from './get-jwt-expiration-date';
 
-const hhmmssToTimestamp =(time: string) => {
-  // expected format: hh:mm:ss
-  let parts = time.split(":");
-  let hours = parseInt(parts[0], 10);
-  let minutes = parseInt(parts[1], 10);
-  let seconds = parseInt(parts[2], 10);
+const hhmmssToTimestamp =(deviceTime: string) => {
+  const deviceTimeInDays = deviceTime.split(' ')[0];
+  const deviceTimeInHours = deviceTime.split(' ')[1];
 
+  // expected format: hh:mm:ss
+  let hoursParts = deviceTimeInHours.split(":");
+  let hours = parseInt(hoursParts[0], 10);
+  let minutes = parseInt(hoursParts[1], 10);
+  let seconds = parseInt(hoursParts[2], 10);
+
+  let DaysParts = deviceTimeInDays.split("/");
+  let d = parseInt(DaysParts[0], 10);
+  let m = parseInt(DaysParts[1], 10);
+  let y = parseInt(DaysParts[2], 10);
+  
   // Create a new Date object with the current year, month, and day
   let date = new Date();
+  date.setDate(d);
+  date.setMonth(m - 1);
+  date.setFullYear(y);
   date.setHours(hours);
   date.setMinutes(minutes);
   date.setSeconds(seconds);
 
   // Return the Unix timestamp 
+  console.log(date)
   return date.getTime();
 }
 
 export const isJwtExpired = (token: string): boolean => {
-  const deviceTime = useSystemData.getState().systemData.timestamp;;
+  const deviceTime = useSystemData.getState().systemData.timestamp;
+  const { lastLoginTime } = useUserData.getState().userData;
+  const halfHour = 1800;
   if (!deviceTime[0]) return false;
 
-  const deviceTimeInHours = deviceTime.split(' ')[1];
-  const expirationDate = getJwtExpirationDate(token);
-
+  console.log(lastLoginTime + halfHour);
+  
   return (
-    expirationDate
-      ? expirationDate < hhmmssToTimestamp(deviceTimeInHours) 
+    lastLoginTime
+      ? lastLoginTime + halfHour >= hhmmssToTimestamp(deviceTime) 
       : true
   );
 }
